22e99b822518d1b085d0a129fd62e4b93c28e742
SONU KUMAR <sonu-2511>
2026-05-04 12:43:55 +0530

    fill the requirement

--- Java_21/src/main/java/com/jb/demo/Demo.java
+++ Java_21/src/main/java/com/jb/demo/Demo.java
@@ -2,6 +2,6 @@ package com.jb.demo;
 
 public class Demo {
 	public static String getMessage() {
-		return "Hello Sonu, How are you"; // added on comment
+		return "Hello Sonu, How are you"; // added on comment but i want one print stmt
 	}
 }
