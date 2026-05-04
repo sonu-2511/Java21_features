ca5b3c3ab11b504fbeb8b54efb623305788c5c64
SONU KUMAR <sonu-2511>
2026-05-04 11:20:09 +0530

    adding rest of the file

---
+++ Java_21/src/test/java/com/jb/Java21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
